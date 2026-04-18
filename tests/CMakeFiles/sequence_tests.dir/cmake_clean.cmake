27322b6961ecf03fbfc513347b5424cc60c9824c
moksanovila-lgtm <moksanovila@gmail.com>
2026-04-18 17:05:53 +0300

    6 commit

---
+++ tests/CMakeFiles/sequence_tests.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sequence_tests.dir/test_array_sequence.cpp.obj"
+  "CMakeFiles/sequence_tests.dir/test_array_sequence.cpp.obj.d"
+  "CMakeFiles/sequence_tests.dir/test_bit_sequence.cpp.obj"
+  "CMakeFiles/sequence_tests.dir/test_bit_sequence.cpp.obj.d"
+  "CMakeFiles/sequence_tests.dir/test_dynamic_array.cpp.obj"
+  "CMakeFiles/sequence_tests.dir/test_dynamic_array.cpp.obj.d"
+  "CMakeFiles/sequence_tests.dir/test_exceptions.cpp.obj"
+  "CMakeFiles/sequence_tests.dir/test_exceptions.cpp.obj.d"
+  "CMakeFiles/sequence_tests.dir/test_immutable.cpp.obj"
+  "CMakeFiles/sequence_tests.dir/test_immutable.cpp.obj.d"
+  "CMakeFiles/sequence_tests.dir/test_linked_list.cpp.obj"
+  "CMakeFiles/sequence_tests.dir/test_linked_list.cpp.obj.d"
+  "CMakeFiles/sequence_tests.dir/test_list_sequence.cpp.obj"
+  "CMakeFiles/sequence_tests.dir/test_list_sequence.cpp.obj.d"
+  "CMakeFiles/sequence_tests.dir/test_map_reduce.cpp.obj"
+  "CMakeFiles/sequence_tests.dir/test_map_reduce.cpp.obj.d"
+  "libsequence_tests.dll.a"
+  "sequence_tests.exe"
+  "sequence_tests.exe.manifest"
+  "sequence_tests.pdb"
+  "sequence_tests[1]_tests.cmake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sequence_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
