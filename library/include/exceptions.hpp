be9671c32bf9b839b011527fb87a49a2f76354e6
moksanovila-lgtm <moksanovila@gmail.com>
2026-04-30 15:37:40 +0300

    14 commit

--- library/include/exceptions.hpp
+++ library/include/exceptions.hpp
@@ -17,49 +17,42 @@ public:
     virtual ~SequenceException() = default;
 };
 
-//выход за границы
 class IndexOutOfRangeException : public SequenceException {
 public:
     explicit IndexOutOfRangeException(const std::string& msg = "Index out of range")
         : SequenceException(msg) {}
 };
 
-//пустая последовательность
 class EmptySequenceException : public SequenceException {
 public:
     explicit EmptySequenceException(const std::string& msg = "Sequence is empty")
         : SequenceException(msg) {}
 };
 
-//попытка изменить неизменяемую последовательность
 class ImmutableModificationException : public SequenceException {
 public:
     explicit ImmutableModificationException(const std::string& msg = "Cannot modify immutable sequence")
         : SequenceException(msg) {}
 };
 
-//неверный аргумент
 class InvalidArgumentException : public SequenceException {
 public:
     explicit InvalidArgumentException(const std::string& msg = "Invalid argument")
         : SequenceException(msg) {}
 };
 
-//состояние итератора
 class IteratorStateException : public SequenceException {
 public:
     explicit IteratorStateException(const std::string& msg = "Iterator out of range")
         : SequenceException(msg) {}
 };
 
-//выделение памяти
 class MemoryAllocationException : public SequenceException {
 public:
     explicit MemoryAllocationException(const std::string& msg = "Memory allocation failed")
         : SequenceException(msg) {}
 };
 
-//несовместимые размеры для битовых операций
 class IncompatibleSizesException : public SequenceException {
 public:
     explicit IncompatibleSizesException(const std::string& msg = "Bit sequences have different sizes")
